e6e0308dca5c50a11d9139c2e029f22c3a4614b7
Zheng Kai <zhengkai@gmail.com>
2015-06-17 17:50:28 +0800

    up

--- Test.php
+++ Test.php
@@ -5,6 +5,8 @@ $f = true;
 
 $a = [];
 
+$foo -> $bar = 1;
+
 $a = 1+2;
 $b= 'c';
 $b ='c';
@@ -23,23 +25,23 @@ function abc1($a=1, $b=2) {
 	echo 'def';
 }
 
-function abc1($a = 1, $b = 2) {
+function abc2($a = 1, $b = 2) {
 	echo 'def';
 }
 
-function abc1( $a = 1) {
+function abc3( $a = 1) {
 	echo 'def';
 }
 
-function abc1( ) {
+function abc4( ) {
 	echo 'def';
 }
 
-function abc3(){
+function abc5(){
 	echo 'def';
 }
 
-function abc4()
+function abc6()
 {
 	echo 'def';
 }
