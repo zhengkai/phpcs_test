96b22b7fa7a2e0c5a28408edfffdd1a0e26d4bfa
Zheng Kai <zhengkai@gmail.com>
2015-06-19 15:41:49 +0800

    up

--- composer-update.sh
+++ composer-update.sh
@@ -12,7 +12,7 @@
 # author: Zheng Kai (zhengkai@gmail.com)
 # 2015-06-12
 
-VENDOR_DIR='tmp-vendor'
+VENDOR_TMP_DIR='tmp-vendor'
 VENDOR_GIT=''
 VENDOR_BRANCH='vendor'
 
@@ -20,6 +20,26 @@ VENDOR_CLEAN_DOTFILES=1
 
 cd $(dirname `readlink -f $0`)
 
+if [ ! -d '.git' ]; then
+	echo 'ERROR: git not found'
+	exit 1
+fi
+
+# 确认 vendor 是否已经被正确的设置为 submodule
+if [ ! -d 'vendor' ]; then
+	if [ -e 'vendor' ]; then
+		echo 'ERROR: "/vendor" is not a directory'
+		exit 1
+	fi
+	git submodule init
+	git submodule update
+fi
+if [ ! -f 'vendor/.git' ]; then
+	echo 'ERROR: no submodule at "/vendor"'
+	exit 1
+fi
+
+# vendor default git url
 if [ -z "$VENDOR_GIT" ]; then
 	VENDOR_GIT=`git config --get remote.origin.url`
 fi
@@ -27,15 +47,18 @@ fi
 echo 'vendor git url: '${VENDOR_GIT}' '${VENDOR_BRANCH}
 echo
 
-if [ ! -d ${VENDOR_DIR} ]; then
-	if [ -e ${VENDOR_DIR} ]; then
-		echo 'ERROR: '${VENDOR_DIR}' is not a directory'
+# check vendor tmp dir exists
+if [ ! -d ${VENDOR_TMP_DIR} ]; then
+	if [ -e ${VENDOR_TMP_DIR} ]; then
+		echo 'ERROR: "/${VENDOR_TMP_DIR}" is not a directory'
 		exit 1
 	fi
-	git clone --single-branch --depth 1 -b ${VENDOR_BRANCH} ${VENDOR_GIT} ${VENDOR_DIR}
+	git clone --single-branch --depth 1 -b ${VENDOR_BRANCH} ${VENDOR_GIT} ${VENDOR_TMP_DIR}
 fi
 
-cd ${VENDOR_DIR}
+cd ${VENDOR_TMP_DIR}
+
+composer update
 
 git co .
 git clean -df
@@ -51,7 +74,7 @@ echo
 echo '                   'run composer update
 echo
 
-composer update
+# composer update
 
 if [ -d vendor ]; then
 	cp -R vendor/* ./
@@ -69,7 +92,9 @@ echo ${DATE}
 # # for debug
 # echo $DATE > datetime.txt
 
-CHANGE=`git status --porcelain | wc -l`
+DO_COMMIT=1
+
+CHANGE=`git status --porcelain 2>&1 | wc -l`
 if [ "$CHANGE" -lt 1 ]; then
 	echo
 	echo '                   'no change, exit
@@ -80,6 +105,27 @@ fi
 echo
 echo '                   'git commit
 echo
+
+# 如果 composer 没有创建过初始的 vendor 目录，
+# 会在第一次运行的时候会以随机hash的形式为 autoload class 起名。
+# 这里是在检查，如果只是 autoload class 名称有变化，则还原，
+# 以避免无意义的 commit
+
+CHECK_AUTOLOAD_ONLY=`git status --porcelain 2>&1 \
+	| grep -v '^\ M\ autoload\.php$' \
+	| grep -v '^\ M\ composer/autoload_real\.php$' \
+	`
+if [ -z "$CHECK_AUTOLOAD_ONLY" ] && [ "$CHANGE" -eq 2  ]; then
+	echo
+	echo '           only autoload class name changed, restore'
+	echo
+	git checkout .
+	cp autoload.php               vendor/autoload.php
+	cp composer/autoload_real.php vendor/composer/autoload_real.php
+	exit
+fi
+
+exit
 
 git add .
 git commit -m "$DATE"
